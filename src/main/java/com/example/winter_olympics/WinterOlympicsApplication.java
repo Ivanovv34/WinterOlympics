9542b61bfe64816f49587ccfe1fbd5ecfdcb3c60
Християн Антонов Иванов <hrisivanov4@gmail.com>
2026-05-02 22:53:23 +0300

    Initial commit and PostgreSQL configuration

---
+++ src/main/java/com/example/winter_olympics/WinterOlympicsApplication.java
@@ -0,0 +1,13 @@
+package com.example.winter_olympics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WinterOlympicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WinterOlympicsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/winter_olympics/WinterOlympicsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.winter_olympics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WinterOlympicsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
